4aafa88addf7beb9ca53a374876ddbd3fd63bf15
thediveo <thediveo@gmx.eu>
2026-03-24 20:44:14 +0000

    test: add examples
    Signed-off-by: thediveo <thediveo@gmx.eu>

--- tuples/doc.go
+++ tuples/doc.go
@@ -1,4 +1,7 @@
 /*
-Package tuple provides mistreating structs as limited tuples.
+Package tuple mistreats structs as a poor man's idea of tuples, using Go
+generics. This package is in no way intended to be a comprehensive tuple
+provider. Instead, it's a bare minimum to pack, pass, and unpack tuples in
+places such as channels where only a single type is expected.
 */
 package tuples
